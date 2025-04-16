cde924473b77739c45b7ec7ca6ffda5ab04381d3
Patrick Morrissey <morrisseypd369@gmail.com>
2025-04-16 00:04:12 -0700

    Test v.3 w/ branch

--- test.cpp
+++ test.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-
+#include <string>
 
 int main() {
+  std::string mood;
 
-  std::cout << "This is a test" << std::endl;
+  std::cout << "Hello Git this is Patrick!" << std::endl;
+  std::cout << "How are you doing today?" << std::endl;
+  std::cin >> mood;
+  std::cout << "Well I hope you have a great rest of your day! :)" << std::endl;
+
   return 0;
-  
 }
